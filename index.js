7afdd403894953315d6b9e093c72325f554318f0
Sanchit <sanchitcop19@Gmail.com>
2019-11-25 05:56:01 -0500

    Added MongoDB support

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const express = require('express')
 const app = express()
+const mongoClient = require('mongodb').MongoClient
+const username = "sanchitcop19";
+const uri = "mongodb+srv://" + username + ":" + process.env.PASSWORD + "@pricelistmanager-hnvmq.mongodb.net/test?retryWrites=true&w=majority";
+const client = new mongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const port = process.env.PORT || 8080
-
-app.use(express.static("public"))
+app.set('json spaces', 4);
+app.use(express.static("public"));
+let store = [];
+client.connect(err => {
+    if (err){
+        return;
+    }
+    const collection = client.db("store").collection("price-list")
+    var cursor = collection.find({});
+    cursor.forEach((doc) => {
+        //console.log(JSON.stringify(doc, null, 4));
+        store.push(doc)
+    }, (err) => {
+        //console.log(err);
+    })
+})
 
 app.get('/store', (req, res) => {
-    res.sendFile('public/store.json', {
+    res.json(store)
+})
+
+app.get('/search', (req, res) => {
+    res.sendFile('public/search.html', {
+        root: __dirname
+    })
+})
+
+app.get('/add', (req, res) => {
+    res.sendFile('public/add.html', {
         root: __dirname
     })
 })
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {
+    res.sendFile('public/index.html', {
         root: __dirname
     })
 })
