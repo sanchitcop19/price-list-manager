fc1e7fab3224807b58a4d7c2901be402150f5a6e
Sanchit <sanchitcop19@Gmail.com>
2019-11-25 14:44:08 -0500

    Basic CRUD functionality

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const body_parser = require('body-parser')
+
+app.use(body_parser.urlencoded(
+    {extended: true}
+))
+
 const mongoClient = require('mongodb').MongoClient
 const username = "sanchitcop19";
 const uri = "mongodb+srv://" + username + ":" + process.env.PASSWORD + "@pricelistmanager-hnvmq.mongodb.net/test?retryWrites=true&w=majority";
@@ -7,17 +13,19 @@ const client = new mongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const port = process.env.PORT || 8080
 app.set('json spaces', 4);
 app.use(express.static("public"));
-let store;
+let store, collection;
 
 
-function update_data(){
+function get_updated_store(){
     store = [];
-    client.connect(err => {
+    client.connect(function(err) {
         if (err){
+            console.log('Error connecting')
             return;
         }
-        const collection = client.db("store").collection("price-list")
+        collection = client.db("store").collection("price-list")
         var cursor = collection.find({});
+        var count = collection.countDocuments();
         cursor.forEach((doc) => {
             //console.log(JSON.stringify(doc, null, 4));
             store.push(doc)
@@ -27,7 +35,11 @@ function update_data(){
     })
 }
 
-update_data();
+function update_store(data){
+    collection.insertOne(data);
+}
+
+get_updated_store();
 
 app.get('/store', (req, res) => {
     res.json(store)
@@ -41,10 +53,26 @@ app.get('/search', (req, res) => {
 
 app.get('/add', (req, res) => {
     // Make sure to update datastore
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.sendFile('public/add.html', {
         root: __dirname
     })
-    update_data();
+})
+
+function create_object(org, price){
+    var object = {}
+    object[org] = parseInt(price);
+    return object;
+}
+
+app.post('/add', (req, res) => {
+    let submission = req.body;
+    let entry = {}
+    console.log(submission)
+    entry["spec"] = submission.specinput;
+    entry["prices"] = create_object(submission.org, submission.price);
+    update_store(entry);
+  res.send('Successful');
 })
 
 app.get('/', (req, res) => {
---
+++ public/add.js
@@ -0,0 +1,95 @@
+function create_field(){
+  
+  let div = document.createElement('div');
+  div.setAttribute('class', 'group');
+  
+  let input = document.createElement('input');
+  input.setAttribute('type', 'text');
+  input.setAttribute('placeholder', 'Organization');
+  input.setAttribute('name', 'org')
+  let price = document.createElement('input');
+  price.setAttribute('type', 'text');
+  price.setAttribute('placeholder', 'Price');
+  price.setAttribute('name', 'price')
+  let span_highlight = document.createElement('span');
+  //span_highlight.setAttribute('class', 'highlight');
+  
+  let label = document.createElement('label');
+  label.innerHTML = "Organization";
+  
+  div.appendChild(input);
+  div.appendChild(price);
+  div.appendChild(span_highlight);
+///  div.appendChild(label);
+  
+  return div;
+}
+
+function add_organization(){
+  let form = document.querySelector('form');
+  form.insertBefore(create_field(), form.childNodes[2])
+}
+
+function remove_organization(){
+  let groups = document.getElementsByClassName('group');
+  
+  let last_child = groups[groups.length-1];
+  if (last_child.id != "specinputdiv"){
+    last_child.parentNode.removeChild(last_child);
+  }
+  
+}
+
+let plus = document.getElementById('plus');
+let minus = document.getElementById('minus');
+ 
+plus.addEventListener('click', add_organization);
+minus.addEventListener('click', remove_organization);
+
+
+/* let submit = document.getElementById('submit-icon');
+
+submit.addEventListener('click', function(){
+  let form = document.querySelector('form#specform');
+  // The first indexing is for the dynamically populated input
+  // The second indexing is for spec/price
+  const specification = form.children[0].children[0].value;
+  console.log(specification);
+  const prices = []
+  const num_data = 2;
+  console.log("form length: " + form.length);
+  for (var i = 1; i < form.length; i = i + num_data){
+    const org = form[i].value;
+    const price = form[i+1].value;
+    const object = {}
+    object[org] = parseInt(price);
+    prices.push(object);
+  }
+  console.log(prices);
+  var entry = {};
+  entry['spec'] = specification;
+  entry['prices'] = prices;
+   var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200){
+      console.log(this.responseText);
+    }
+    xhttp.open('POST', 'http://localhost:8080/add', true);
+    xhttp.setRequestHeader('Content-Type', 'application/json')
+    xhttp.send(entry);
+  } 
+  const url = "http://localhost:8080/add";
+  const params = {
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: entry,
+    method: "POST",
+    mode: "no-cors"
+  }
+  fetch(url, params)
+  .then(data => console.log("fuck1" + data))
+  .then(res => console.log("fuck2" + res))
+  .catch(err => console.log("fuck3" + err))
+  //window.location.href = "https://price-list-manager.herokuapp.com/";
+}) */
